4d9c37525b29ca6bb871e29fd4f19bf650dc4301
Carlos Eduardo <carlosedbalmeida@live.com>
2015-08-22 17:52:20 -0300

    tests

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -43,39 +43,49 @@ var ACE2D = ACE2D || function () {
 	var azul = new Color(50, 60, 160);
 
 	// Chão
-	var cube = Shape.Prism(Point.ORIGIN, 20, 20, 0.5);
+	var cube = Shape.Prism(Point.ORIGIN, 20, 20, 0.5)
+	  .rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 2.5)
 	iso.add(cube);
 
-	iso.add(Shape.Prism(new Point(0, 15, 0), 20, 5, 6)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(0, 16, 0), 20, 4, 6)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, azul);
 
-	iso.add(Shape.Prism(new Point(0, 0, 0), 20, 5, 6)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(0, 0, 0), 20, 4, 6)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, azul);
 
-	iso.add(Shape.Prism(new Point(15, 14.8, 0), 3, 0.2, 3.5)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(15, 15.8, 0), 3, 0.2, 3.5)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, gay);
 
-	iso.add(Shape.Prism(new Point(12, 14.8, 0), 3, 0.2, 3.5)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(12, 15.8, 0), 3, 0.2, 3.5)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, azulverde);
 
-	iso.add(Shape.Prism(new Point(9, 14.8, 0), 3, 0.2, 3.5)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(9, 15.8, 0), 3, 0.2, 3.5)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, gay);
 
-	iso.add(Shape.Prism(new Point(6, 14.8, 0), 3, 0.2, 3.5)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(6, 15.8, 0), 3, 0.2, 3.5)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, azulverde);
 
-	iso.add(Shape.Prism(new Point(3, 14.8, 0), 3, 0.2, 3.5)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(3, 15.8, 0), 3, 0.2, 3.5)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, gay);
 
-	iso.add(Shape.Prism(new Point(0, 14.8, 0), 3, 0.2, 3.5)
-	  .translate(0, 0, 0.5)
+	iso.add(Shape.Prism(new Point(0, 15.8, 0), 3, 0.2, 3.5)
+		.rotateZ(Point(1.5, 1.5, 0), Math.PI / 12)
+	  .translate(0, -2.5, 3)
 	, azulverde);
 
 };
